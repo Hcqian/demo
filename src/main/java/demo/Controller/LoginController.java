e1b4b73eb8176e5b84b2db7dde2bfb67abf5659c
haochenqian <email@example.com>
2017-12-06 10:36:09 +0800

    test

--- src/main/java/demo/Controller/LoginController.java
+++ src/main/java/demo/Controller/LoginController.java
@@ -6,13 +6,11 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Controller
 @RequestMapping("/")
 public class LoginController {
+
 
     @RequestMapping("index")
     public String index(){
--- src/main/java/demo/intercept/DemoInterceptor.java
+++ src/main/java/demo/intercept/DemoInterceptor.java
@@ -9,13 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 public class DemoInterceptor implements HandlerInterceptor {
 
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
-        System.out.println("11111111111111");
-        System.out.println(request.getClass().getName()+","+response.getClass().getName());
-        System.out.println(request.getRequestURI()+","+request.getRequestURL()+","+
-                request.getContextPath()+","+request.getServletPath()
-                +","+request.getServerName());
-
-//        response.sendRedirect("/login.htm");
         return true;
     }
 
